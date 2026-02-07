8ec9e7352dabc64aedaf28a8c84472346dd86970
Ernela <fenixoffc@gmail.com>
2026-02-07 19:52:12 +0200

    Fix build

--- app/src/main/java/com/neo/neomovies/ui/watch/WatchSelectorScreen.kt
+++ app/src/main/java/com/neo/neomovies/ui/watch/WatchSelectorScreen.kt
@@ -40,10 +40,12 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.onDispose
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
